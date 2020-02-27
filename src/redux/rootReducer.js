9a965ce4c55f333a60ce9c494d9fbc3bf9589f6f
riyaistar <riyamitra@istarindia.com>
2020-02-28 04:53:39 +0530

    changes logging

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import cakeReducer from './cake/cakeReducer'
 import icecreamReducer from './icecream/icecreamReducer'
 
-
 const rootReducer = combineReducers({
     cake: cakeReducer,
     icecream: icecreamReducer
